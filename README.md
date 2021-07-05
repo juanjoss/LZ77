ff872b1bb4e713d96880342a23e46b9dbfc1ebad
fuato1 <fuatojfj@gmail.com>
2021-07-05 11:00:11 -0300

    README.md mod.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LZ77 Compression Algorithm.
 
-LZ77 is a text compression algorithm, dictorinary based, which uses a sliding window to encode previously seen data.
+LZ77 is a lossless compression algorithm, dictorinary based, which uses a sliding window to encode previously seen data.
 
 This implementation is written in Go as an intention to improve my skills.
 
@@ -9,10 +9,18 @@ This implementation is written in Go as an intention to improve my skills.
 ***Not yet.***
 
 ## How to use it
-
-    This will create the executable and clean the files generated from the test.
 > make
+
+    This will create the executable and clean the files generated from the tests.
+
+> ./lz77
 
     Run it! 
     For now it just takes the lorem.txt in the test_files folder, and creates a compressed (lorem_comp) and a decompressed file (lorem).
-> ./lz77
+
+## Credits
+
+    I used this resources to understand the real implementation of LZ77. Since in reality bit operations are needed to encode and decode data.
+
+- https://github.com/fbonhomm/LZ77
+- https://www.youtube.com/watch?v=zIwTq2yPCU4&t=1297s
--- main.go
+++ main.go
@@ -3,23 +3,39 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+
+	lz77 "github.com/fuato1/lz77/LZ77"
 )
 
 func main() {
-	data, err := ioutil.ReadFile("./lorem.txt")
+	// reading file
+	data, err := ioutil.ReadFile("./test_files/lorem.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	lz77 := Init()
+	// compressing data
+	lz77 := lz77.Init()
 	compressedData := lz77.Compress(data)
+
+	// stats
 	compressionRatio := float64(len(compressedData)) / float64(len(data))
 
 	fmt.Println("Original size: ", len(data))
 	fmt.Println("Compressed size: ", len(compressedData))
-	fmt.Println("Compression ratio: ", compressionRatio)
+	fmt.Printf("Compression ratio: %.2f\n", compressionRatio)
 
-	err = ioutil.WriteFile("./lorem.lz77", compressedData, 0644)
+	// writing compressed data
+	err = ioutil.WriteFile("./test_files/lorem_comp", compressedData, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// decompressing data
+	decompressedData := lz77.Decompress(compressedData)
+
+	// writing decompressed data
+	err = ioutil.WriteFile("./test_files/lorem", decompressedData, 0644)
 	if err != nil {
 		panic(err)
 	}
